243d0156aa2a7a73704356503dd5ab18c0670ca7
Abdul Muqsit <92204326+amuqsit57@users.noreply.github.com>
2023-09-10 13:45:55 +0100

    Add files via upload

---
+++ docs/App.js
@@ -0,0 +1,23 @@
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/header"
+import Middle from "./components/middle/middle"
+import Partners from "./components/partners/Partners"
+import Properties from "./components/properties/Properties";
+
+function App() {
+  return (
+    <div>
+      
+      <Header/>
+      <Middle/>
+      <Partners/>
+    <Properties/>
+    <Contact/>
+    <Footer/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ docs/components/contact/Contact.js
@@ -0,0 +1,127 @@
+import React from 'react'
+
+import "./Contact.css"
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      
+      
+
+        <div className="left_middles">
+
+
+        <div className="text">
+        <p className='t1'>Contact Us</p>
+        <p className='t2'>Easy to contact us</p>
+        <p className='t3'>We always ready to help by providijng the best services for you. We beleive a good blace to live can make your life better</p>
+        </div>
+        
+        <div className="boxes">
+
+
+
+            <div className="box_up">
+
+
+                
+
+
+
+
+
+
+                <div className="box">
+                    <div className="section1">
+                    <img src="https://th.bing.com/th/id/OIP.EXZ5XOelzuWXARIC7nkxUQHaHa?pid=ImgDet&w=182&h=182&c=7&dpr=1.3" alt="" />
+
+                        <div className="texts">
+                            <p>Call</p>
+                            <p>021 123 142 12</p>
+
+                        </div>
+                    </div>
+                    <div className="section2">
+                        <button className='btn'>Call now</button>
+                    </div>
+                    
+                </div>
+
+
+                <div className="box">
+                    <div className="section1">
+                    <img src="https://cdn0.iconfinder.com/data/icons/semgeler-social/512/Comment-256.png" alt="" />
+
+                        <div className="texts">
+                            <p>Email</p>
+                            <p>021 123 142 12</p>
+
+                        </div>
+                    </div>
+                    <div className="section2">
+                        <button className='btn'>Call now</button>
+                    </div>
+                    
+                </div>
+            
+
+            </div>    
+
+            <div className="box_down">
+          
+
+            <div className="box">
+                    <div className="section1">
+                    <img src="https://play-lh.googleusercontent.com/1L09eXYU8PIKq65xLKM_mlIav-Jzb_mp8F-fKwjRcOtDV52S5_SysdfaYy3gOk-8ASM=s180" alt="" />
+
+                        <div className="texts">
+                            <p>Video</p>
+                            <p>021 123 142 12</p>
+
+                        </div>
+                    </div>
+                    <div className="section2">
+                        <button className='btn'>Call now</button>
+                    </div>
+                    
+                </div>
+
+                
+                <div className="box">
+                    <div className="section1">
+                    <img src="https://th.bing.com/th/id/R.3ea7bc46836008c4a1f78dc4f0b6721b?rik=Ii1DW3OxDQpOtw&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2011%2f08%2femail-message-icon_296856.png&ehk=O4QnefBD9yR16LfhXKChtSZnlBJxBQrniC9EWB3uKMM%3d&risl=&pid=ImgRaw&r=0" alt="" />
+
+                        <div className="texts">
+                            <p>Message</p>
+                            <p>021 123 142 12</p>
+
+                        </div>
+                    </div>
+                    <div className="section2">
+                        <button className='btn'>Call now</button>
+                    </div>
+                    
+                </div>
+            </div>
+
+            
+
+        </div>
+
+        
+           
+
+        </div>
+        <div className="r_midd">
+
+            <div className="middle_imag">
+
+
+            </div>
+        </div>
+      </div>
+    
+  )
+}
+
+export default Contact
